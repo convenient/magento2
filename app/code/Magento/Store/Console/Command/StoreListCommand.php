e2f0a022ceb5d35e953d0f71625f51d8b9add779
Luke Rodgers <lukerodgers90@gmail.com>
2016-12-28 18:58:53 +0000

    Add store magento commands to list data
    ```
    $bin/magento store:website:list
    +----+------------------+--------------+-------+------------+------------+
    | ID | Default Group Id | Name         | Code  | Sort Order | Is Default |
    +----+------------------+--------------+-------+------------+------------+
    | 0  | 0                | Admin        | admin | 0          | 0          |
    | 1  | 1                | Main Website | base  | 0          | 1          |
    | 2  | 2                | Other Site   | other | 0          | 0          |
    +----+------------------+--------------+-------+------------+------------+
    
    $bin/magento store:list
    +----+------------+----------+--------------------+---------+------------+-----------+
    | ID | Website ID | Group ID | Name               | Code    | Sort Order | Is Active |
    +----+------------+----------+--------------------+---------+------------+-----------+
    | 0  | 0          | 0        | Admin              | admin   | 0          | 1         |
    | 1  | 1          | 1        | Default Store View | default | 0          | 1         |
    | 2  | 2          | 2        | Other Store View   | other   | 0          | 1         |
    +----+------------+----------+--------------------+---------+------------+-----------+
    ```

---
+++ app/code/Magento/Store/Console/Command/StoreListCommand.php
@@ -0,0 +1,58 @@
+<?php
+namespace Magento\Store\Console\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Command\Command;
+
+/**
+ * Class StoreListCommand
+ * @package Magento\Store\Console\Command
+ */
+class StoreListCommand extends Command
+{
+    /** @var \Magento\Store\Model\StoreManagerInterface $storeManager */
+    private $storeManager;
+
+    public function __construct(
+        \Magento\Store\Model\StoreManagerInterface $storeManager
+    )
+    {
+        $this->storeManager = $storeManager;
+        parent::__construct();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setName('store:list')
+            ->setDescription('Displays the list of stores');
+
+        parent::configure();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $table = $this->getHelperSet()->get('table');
+        $table->setHeaders(['ID', 'Website ID', 'Group ID', 'Name', 'Code', 'Sort Order', 'Is Active']);
+
+        foreach ($this->storeManager->getStores(true, true) as $store) {
+            $table->addRow([
+                $store->getId(),
+                $store->getWebsiteId(),
+                $store->getStoreGroupId(),
+                $store->getName(),
+                $store->getCode(),
+                $store->getData('sort_order'),
+                $store->getData('is_active'),
+            ]);
+        }
+
+        $table->render($output);
+    }
+}
